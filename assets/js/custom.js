c90f69e974b352d491a939a24a85df037e92542d
patribezek <patribezek@gmail.com>
2018-07-21 19:23:21 +0200

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,24 @@
+
+
+
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  var distance = 14400000;
+
+  // Time calculations for days, hours, minutes and seconds
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("countdown").innerHTML = hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
